71b0105232aae762da86ce82f948c81501b539f8
Kiran M. S <71029691+Stark0204@users.noreply.github.com>
2023-01-27 13:32:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='RSNA_torch',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['numpy','pandas', 'torchvision', 'torch', 'pytorch_lightning', 'hydra', "python-gdcm", "pydicom", "pylibjpeg"], # list of dependencies
+    install_requires=['numpy','pandas', 'torchvision', 'torch', 'pytorch_lightning', 'hydra', "python-gdcm", "pydicom", "pylibjpeg", "omegaconf"], # list of dependencies
     # other options
 )
