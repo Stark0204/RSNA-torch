1d7ed775cb6d4d3c44f1bf987105e873b086589a
Kiran M. S <71029691+Stark0204@users.noreply.github.com>
2023-01-27 13:56:32 +0100

    Add files via upload

--- RSNA_torch/RSNAlightning.py
+++ RSNA_torch/RSNAlightning.py
@@ -8,8 +8,6 @@ import pytorch_lightning as pl
 from logging import getLogger
 from pytorch_lightning.utilities.types import EPOCH_OUTPUT
 
-from F1Score.P_F1 import ProbF1Score
-
 log = getLogger(__name__)
 
 
@@ -149,3 +147,36 @@ class RSNAlighningModule(pl.LightningModule):
         )
 
 
+def pf1beta(predictions, labels, beta):
+    y_true_count = 0
+    ctp = 0
+    cfp = 0
+
+    for idx in range(len(labels)):
+        prediction = min(max(predictions[idx], 0), 1)
+        if labels[idx]:
+            y_true_count += 1
+            ctp += prediction
+        else:
+            cfp += prediction
+
+    beta_squared = beta ** 2
+    c_precision = ctp / (ctp + cfp)
+    c_recall = ctp / y_true_count
+    if c_precision > 0 and c_recall > 0:
+        result = (1 + beta_squared) * (c_precision * c_recall) / (beta_squared * c_precision + c_recall)
+        return result
+    else:
+        return 0
+
+
+class ProbF1Score:
+    def __init__(self, predictions: torch.Tensor, labels: torch.Tensor, beta: int = 1):
+        self.predictions = predictions
+        self.labels = labels
+        self.beta = beta
+
+        self.score = pf1beta(self.predictions, self.labels, self.beta)
+
+    def f1_score(self):
+        return self.score
--- RSNA_torch/__init__.py
+++ RSNA_torch/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '0.1.0'
-from . import config, data, RSNAlightning, utils, F1Score
+from . import config, data, RSNAlightning, utils
 
-__all__ = ["config", "data", "RSNAlightning", "utils", "F1Score"]
+__all__ = ["config", "data", "RSNAlightning", "utils"]
